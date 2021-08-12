25797879773bdc69c1dbc488abac77c11e0ac908
Sayuri Kamble <68416000+SAYUK09@users.noreply.github.com>
2021-08-12 21:28:51 +0530

    Removed comments

--- server.ts
+++ server.ts
@@ -1,14 +1,3 @@
-// const express = require("express");
-// const app = express();
-// const server = require("http").Server(app);
-// const next = require("next");
-// const dev = process.env.NODE_ENV !== "production";
-// const nextApp = next({ dev });
-// const handle = nextApp.getRequestHandler();
-// require("dotenv").config();
-// app.use(express.json());
-// const PORT = process.env.PORT || 3000;
-
 import dotenv from "dotenv";
 dotenv.config();
 import express from "express";
@@ -20,10 +9,9 @@ const handle = nextApp.getRequestHandler();
 const app = express();
 const PORT = process.env.PORT || 3000;
 const server = require("http").Server(app);
-const swndmail = require("./pages/api/sendmail");
 
 nextApp.prepare().then(() => {
-  app.use("/api/sendmails", swndmail);
+  app.use("/api/sendmails", require("./pages/api/sendmail"));
   app.all("*", (req: any, res: any) => handle(req, res));
 
   server.listen(PORT, (err: any) => {
