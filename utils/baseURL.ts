b2cdf8a50b996c31f755a0697994f3f6f1390a42
Sayuri Kamble <68416000+SAYUK09@users.noreply.github.com>
2021-09-03 16:44:33 +0530

    deployment fix

--- utils/baseURL.ts
+++ utils/baseURL.ts
@@ -1,4 +1,4 @@
 export const baseUrl =
   process.env.NODE_ENV !== "production"
     ? "http://localhost:3000"
-    : "https://mail-sender-portal.vercel.app";
+    : "https://mail-senderr.herokuapp.com";
