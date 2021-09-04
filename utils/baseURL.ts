f4d23bbe25f6f8fce1f628c99444fa7da3216c22
Sayuri Kamble <68416000+SAYUK09@users.noreply.github.com>
2021-09-05 00:45:51 +0530

    deployment fix

--- utils/baseURL.ts
+++ utils/baseURL.ts
@@ -1,4 +1,4 @@
 export const baseUrl =
   process.env.NODE_ENV !== "production"
     ? "http://localhost:3000"
-    : "https://mails-senderr.herokuapp.com/";
+    : "https://mails-senderr.herokuapp.com";
